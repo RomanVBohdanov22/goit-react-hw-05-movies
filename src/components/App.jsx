94d41efe91b5ac1bbe6a7fde8e8338d94d60b99c
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-05-05 22:43:53 +0300

    Naj bude
    I will see some new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+/* API Key: c20d992e26e4f0d24c347a36a85c7acc */
+/* https://api.themoviedb.org/3/movie/550?api_key=c20d992e26e4f0d24c347a36a85c7acc */
 export const App = () => {
   return (
     <div
