a2e9756e71b2a3c39acb86f24df9d9369289dafe
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-05-06 16:01:31 +0300

    some tests I'm doing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 /* API Key: c20d992e26e4f0d24c347a36a85c7acc */
 /* https://api.themoviedb.org/3/movie/550?api_key=c20d992e26e4f0d24c347a36a85c7acc */
+/* https://api.themoviedb.org/3/movie/400?api_key=c20d992e26e4f0d24c347a36a85c7acc */
+/*const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjBkOTkyZTI2ZTRmMGQyNGMzNDdhMzZhODVjN2FjYyIsInN1YiI6IjY0NTU0NzU5NzEwODNhMDBlM2Y2ZmM0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QffZw7Vr_Ul4cqQEDvXNhketX3RjlGUEi8XqjIFuOss'
+  }
+};
+fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));*
+  */
+
 export const App = () => {
   return (
     <div
